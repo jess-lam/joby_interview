956a253728ab41577f2f0431b8ceeb709458708f
Jessica Sunding <jessicasunding@jessicas-mbp.lan>
2025-12-10 22:55:39 -0800

    add routes to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import IssueListPage from './pages/IssueListPage'
+import CreateIssuePage from './pages/CreateIssuePage'
+import ShowIssuePage from './pages/ShowIssuePage'
+import EditIssuePage from './pages/EditIssuePage'
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/issues/new" element={<CreateIssuePage />} />
+        <Route path="/issues/:id/edit" element={<EditIssuePage />} />
+        <Route path="/issues/:id" element={<ShowIssuePage />} />
         <Route path="/issues" element={<IssueListPage />} />
       </Routes>
     </BrowserRouter>
